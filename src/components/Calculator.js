71a598150cffabfa5376ae7f9c6518c05a807551
TatyCat <williams.tatyanna@gmail.com>
2019-05-07 17:15:27 -0400

    Calculation Functionality Added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,20 +5,46 @@ class Calculator extends Component {
     super(props)
 
     this.state = {
-      result: ""
+      result: "",
+      numberPressed: "",
+      numberSet: [],
+      operator: []
     }
   }
 
   clearCalculator = () => {
     this.setState({ result: "" })
+    this.setState({ numberPressed: "" })
+    this.setState({ numberSet: [] })
+    this.setState({ operator: [] })
+  }
+  componentDidMount() { this.clearCalculator() }
+
+  addNumber = (e) => {
+    if (this.state.result !== "") {
+      this.clearCalculator()
+      this.setState({ numberPressed: this.state.numberPressed + e.target.innerText })
+
+    }
+    else if (this.state.result === "") {
+      this.setState({ numberPressed: this.state.numberPressed + e.target.innerText })
+    }
   }
 
-  buttonClicked = (e) => {
-    this.setState({ result: this.state.result + e.target.innerText })
+  operator = (addOperator) => {
+    this.setState({ numberSet: this.state.numberPressed })
+    this.setState({ numberPressed: "" })
+    this.setState({ operator: addOperator })
   }
 
   calculate = () => {
-    console.log(this.state.result)
+    if (this.state.result === "") {
+      this.setState({ numberSet: this.state.numberSet.concat(this.state.numberPressed) })
+
+      this.setState({ numberPressed: "" })
+
+      this.setState({ result: eval(`${this.state.numberSet} ${this.state.operator} ${this.state.numberPressed}`) })
+    }
   }
 
   render() {
@@ -27,28 +53,28 @@ class Calculator extends Component {
         <article id="calculator">
           <div className="top">
             <span className="top-screen"></span>
-            <div className="input-screen">{this.state.result}</div>
+            <div className="input-screen">{this.state.numberPressed}{this.state.result}</div>
           </div>
 
           <div className="keys">
             <button className="clear" onClick={this.clearCalculator}>ac</button>
             <button onClick={this.buttonClicked}>+/-</button>
             <button onClick={this.buttonClicked}>%</button>
-            <button className="operator" onClick={this.buttonClicked}>÷</button>
-            <button onClick={this.buttonClicked}>7</button>
-            <button onClick={this.buttonClicked}>8</button>
-            <button onClick={this.buttonClicked}>9</button>
-            <button className="operator" onClick={this.buttonClicked}>x</button>
-            <button onClick={this.buttonClicked}>4</button>
-            <button onClick={this.buttonClicked}>5</button>
-            <button onClick={this.buttonClicked}>6</button>
-            <button className="operator" onClick={this.buttonClicked}>-</button>
-            <button onClick={this.buttonClicked}>1</button>
-            <button onClick={this.buttonClicked}>2</button>
-            <button onClick={this.buttonClicked}>3</button>
-            <button className="operator" onClick={this.buttonClicked}>+</button>
-            <button onClick={this.buttonClicked}>0</button>
-            <button onClick={this.buttonClicked}>.</button>
+            <button className="operator" onClick={() => { this.operator("/") }}>÷</button>
+            <button onClick={this.addNumber}>1</button>
+            <button onClick={this.addNumber}>2</button>
+            <button onClick={this.addNumber}>3</button>
+            <button className="operator" onClick={() => { this.operator("*") }}>x</button>
+            <button onClick={this.addNumber}>4</button>
+            <button onClick={this.addNumber}>5</button>
+            <button onClick={this.addNumber}>6</button>
+            <button className="operator" onClick={() => { this.operator("-") }}>-</button>
+            <button onClick={this.addNumber}>7</button>
+            <button onClick={this.addNumber}>8</button>
+            <button onClick={this.addNumber}>9</button>
+            <button className="operator" onClick={() => { this.operator("+") }}>+</button>
+            <button onClick={this.addNumber}>0</button>
+            <button onClick={this.addNumber}>.</button>
             <button className="operator double-space" onClick={this.calculate}>=</button>
           </div>
         </article >
